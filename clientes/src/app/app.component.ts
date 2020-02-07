310bee69b4bfe68af8ce2f29c285ffa2e7ae21b3
Leo <leoonorio_02@hotmail.com>
2020-02-07 18:49:10 -0500

    Se creo el proyecto de angular, se adiciono Bootstrap y Navbar

---
+++ clientes/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Klaus';
+  empresa: string='Klaus Leather Store';
+  software: string='Hmnos Bateca';
+}
