a607412cb0fa81246f471deda027bbf2f69d9197
Leo <leoonorio_02@hotmail.com>
2020-02-14 17:06:18 -0500

    Se creo service:sejecuta los metodos y hace la coneccion con la url, clase cliente, el componente cliente que crea la tabla, componente formulario crea el formulario

--- clientes/src/app/app.module.ts
+++ clientes/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { ClienteService } from './clientes/cliente.service';
 
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { FormComponent } from './clientes/form.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes:Routes =[
+  //{path:'', redirecto: '/clientes', pathMatch:'full'},//home
+    {path: '',redirectTo: '/clientes', pathMatch: 'full'},
+    {path: 'clientes', component:ClientesComponent},
+    {path: 'clientes/form', component:FormComponent},
+    {path: 'clientes/form/:id', component:FormComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    FormComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
